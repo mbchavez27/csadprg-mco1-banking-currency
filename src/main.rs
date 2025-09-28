85800a9e5045756b6d5fad227cc31cb77f182518
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2025-09-29 01:13:42 +0800

    feat: added main menu

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    print_menu();
+}
+
+fn print_menu(){
+    println!("Select Transaction:");
+    println!("[1] Register Account Name");
+    println!("[2] Deposit Amount");
+    println!("[3] Withdraw Amount");
+    println!("[4] Currency Exchange");
+    println!("[5] Record Exchange Rates");
+    println!("[6] Show Intereest Computation");
 }
